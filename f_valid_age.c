bfbc2713383086cbbc2a3e45a7d9a0531ab4ffe9
MikeK <kurshakov.mihail@gmail.com>
2016-12-16 14:59:20 +0200

    f_valid_age

---
+++ f_valid_age.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Car{
+    int  N;
+    char car_number[10];
+} ;
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
+
+bool f_valid_N( * char str)
+{
+    printf("was age valid");
+    return true;
+}
