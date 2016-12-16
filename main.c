3635c6ef191dba18c6001c294e18b2962355c5be
MikeK <kurshakov.mihail@gmail.com>
2016-12-16 14:54:52 +0200

    Human

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Human{
+    int  age;
+    char last_name[30];
+    char first_name[30];
+} ;
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
